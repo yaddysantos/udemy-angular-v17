dbec54887cbb4eb25f58053e4f0bf72e53ad49d3
yaddysantos <34655963+yaddysantos@users.noreply.github.com>
2024-03-11 16:38:16 -0500

    Method FOR control flow

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -11,7 +11,8 @@ type Grade = 'A' | 'B' | 'C';
 export class ControlFlowComponent {
   public showContent = signal(false);
   public grade = signal<Grade>('A');
-  public frameworks = signal(['Angular', 'Vue', 'Svelte'])
+  public frameworks = signal(['Angular', 'Vue', 'Svelte', 'Qwik', ' React'])
+  public frameworks2 = signal([])
 
   public toggleContent() {
     this.showContent.update((value) => !value);
