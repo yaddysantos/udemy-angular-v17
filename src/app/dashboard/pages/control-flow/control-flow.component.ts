4974d2a982fa1faae800e0462c8ab929309c6c9d
yaddysantos <34655963+yaddysantos@users.noreply.github.com>
2024-03-11 11:54:27 -0500

    insercion control flow

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-control-flow',
   standalone: true,
   imports: [],
   templateUrl: './control-flow.component.html',
-  styleUrl: './control-flow.component.css'
+  styleUrl: './control-flow.component.css',
 })
 export class ControlFlowComponent {
+  public showContent = signal(false);
 
+  public toggleContent() {
+    this.showContent.update((value) => !value);
+  }
 }
