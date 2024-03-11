be379e7f40fe2f70a99afcb0b13ab893b1a83a56
yaddysantos <34655963+yaddysantos@users.noreply.github.com>
2024-03-11 17:44:17 -0500

    adicion de los types defer

--- src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { HeavyLoadersSlowComponent } from '@share/heavy-loaders/heavy-loaders-slow.component';
+import { TitleComponent } from '@share/title/title.component';
 
 @Component({
   selector: 'app-defer-views',
   standalone: true,
-  imports: [CommonModule, HeavyLoadersSlowComponent],
+  imports: [CommonModule, HeavyLoadersSlowComponent, TitleComponent],
   templateUrl: './defer-views.component.html'
 })
 export class DeferViewsComponent {
