9beb84e801959caf4cff901bee978a8838858800
yaddysantos <34655963+yaddysantos@users.noreply.github.com>
2024-03-11 11:39:28 -0500

    Sintaxis Ng For Side Menu
    inclusion del NgFor con los elementos traidos del route

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import { routes } from '../../app.routes';
-
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-sidemenu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidemenu.component.html',
-  styleUrl: './sidemenu.component.css'
+  styleUrl: './sidemenu.component.css',
 })
 export class SidemenuComponent {
+  public menuItems = routes
+    .map((route) => route.children ?? [])
+    .flat() //Filter routes in only one array;
+    .filter((route) => route && route.path) //Filter all routes
+    .filter((route) => !route.path?.includes(':')); //Exlude path user/:id
 
-  public menuItems = routes.map(route => route.children ?? [])
-  .flat()
-  .filter( route => route && route.path)
-  .filter(route => !route.path?.includes(':'))
-
-  constructor(){}
+  constructor() {
+  }
 }
