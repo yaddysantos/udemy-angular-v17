26d46b760184bf24a804e6241f7b4efec2330ca5
yaddysantos <34655963+yaddysantos@users.noreply.github.com>
2024-03-11 17:23:39 -0500

    Creacion change detection signal
    Mejora el performance de angular

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  signal,
+} from '@angular/core';
+import { TitleComponent } from '@share/title/title.component';
 
 @Component({
   selector: 'app-change-detection',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TitleComponent],
   templateUrl: './change-detection.component.html',
-  styleUrl: './change-detection.component.css'
+  styleUrl: './change-detection.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush, //esta pendiente menos ciclos de vida o puntos de cambios en la aplicacion
 })
 export class ChangeDetectionComponent {
+  public currentFramework = computed(
+    () => `Change detection - ${this.frameworkAsSignal().name}`
+  );
 
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  });
+
+  public frameworkAsProperty = {
+    name: 'Angular 17',
+    releaseDate: 2016,
+  };
+
+  constructor() {
+    setTimeout(() => {
+      // this.frameworkAsProperty.name = 'React'
+      this.frameworkAsSignal.update((value) => ({ ...value, name: 'React' })); // Signal permite a angular que sea mas rapido
+      console.log('done');
+    }, 3000);
+  }
 }
